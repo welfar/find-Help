12dcd20645bce95d844d564d09b78dc9252df4d5
Welfar <jdwelfar91@gmail.com>
2021-08-06 10:57:37 -0500

    Se agregan dependencias

---
+++ src/components/HeaderNav.js
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+
+function HeaderNav() {
+	return (
+		<nav>
+			<ul>
+				<li>
+					<Link to="/">Fundaciones</Link>
+				</li>
+			</ul>
+		</nav>
+	);
+}
+export default HeaderNav;
---
+++ src/pages/HomeView.js
@@ -0,0 +1,12 @@
+import HeaderNav from "../components/HeaderNav";
+import "../styles/HomeView.css";
+
+function HomeView() {
+	return (
+		<div>
+			<HeaderNav />
+		</div>
+	);
+}
+
+export default HomeView;
